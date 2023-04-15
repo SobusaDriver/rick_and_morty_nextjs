0b3e4d5912daa63ea7936d3dc1eb29741a0d91e0
Keneth Lallemand <keneth.ortegal@perficient.com>
2023-04-15 15:10:05 -0500

    added static path implementation

--- src/pages/episodes/[id].tsx
+++ src/pages/episodes/[id].tsx
@@ -5,7 +5,7 @@ import {
 	EPISODE_COMPLEMENT,
 	PAGINATION_COMPLEMENT,
 } from "@/constants";
-import { GetServerSideProps } from "next";
+import { GetStaticPaths, GetStaticProps } from "next";
 import { ParsedUrlQuery } from "querystring";
 import React from "react";
 import Episode from "@/models/Episode";
@@ -20,7 +20,27 @@ interface locationProps {
 interface Params extends ParsedUrlQuery {
 	id: string;
 }
-export const getServerSideProps: GetServerSideProps = async (context) => {
+
+export const getStaticPaths: GetStaticPaths<Params> = async () => {
+	const req = await fetch(`${API_URL}${EPISODE_COMPLEMENT}${PAGINATION_COMPLEMENT}1`);
+	const req_parsed = await req.json();
+	const paths: Array<object> = [];
+	for (let i = 1; i <= req_parsed.info.pages; i++) {
+		paths.push({
+			params: {
+				id: `${i}`
+			}
+		});
+	}
+	return {
+		paths,
+		fallback: 'blocking'
+	}
+
+}
+
+
+export const getStaticProps: GetStaticProps = async (context) => {
 	const { id } = context.params as Params;
 
 	const req = await fetch(
@@ -46,7 +66,7 @@ const LocationPage = ({
 		<main className="h-screen flex flex-col">
 			<Navbar />
 			<h3 className="p-6 mb-1 text-4xl font-medium leading-tight">
-				List Of Locations
+				List Of Episodes
 			</h3>
 			<div className="flex-1">
 				<CardsContainer ListOfEpisodes={listOfEpisodes} />
